7c69a88944413e9d0866b55df1f2d33a2d804656
lee31 <sai6321@naver.com>
2023-02-13 23:21:16 +0900

    #21 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영

--- src/main/java/com/fastcampus/boardproject/controller/ArticleController.java
+++ src/main/java/com/fastcampus/boardproject/controller/ArticleController.java
@@ -2,9 +2,10 @@ package com.fastcampus.boardproject.controller;
 
 import com.fastcampus.boardproject.domain.dto.response.*;
 import com.fastcampus.boardproject.domain.type.*;
-import com.fastcampus.boardproject.service.ArticleService;
+import com.fastcampus.boardproject.service.*;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -28,9 +32,11 @@ public class ArticleController {
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles",
-                articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from)
-        );
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
